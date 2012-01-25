864d90088fcc3a67dbc1f1fe319f8b82492fc5c0
Andrew Hart <andrew@homer.(none)>
2012-01-24 21:16:03 -0800

    update the app to do what we want

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,52 +5,29 @@ run(function () {
     // immediately invoked on first run
     var init = (function () {
         if (navigator.network.connection.type == Connection.NONE) {
-            alert("No internet connection - we won't be able to show you any maps");
+            alert("You have no internet connection. I have no idea where you are!");
         } else {
-            alert("We can reach Google - get ready for some awesome maps!");
+            alert("You have internet! There's at least a chance I can tell you where you are!");
         }
     })();
     
     // a little inline controller
     when('#welcome');
-    when('#settings', function() {
-		// load settings from store and make sure we persist radio buttons.
-		store.get('config', function(saved) {
-			if (saved) {
-				if (saved.map) {
-					x$('input[value=' + saved.map + ']').attr('checked',true);
-				}
-				if (saved.zoom) {
-					x$('input[name=zoom][value="' + saved.zoom + '"]').attr('checked',true);
-				}
-			}
-		});
-	});
+    
     when('#map', function () {
         store.get('config', function (saved) {
-            // construct a gmap str
+            // construct a datafluency geo request
             var map  = saved ? saved.map || ui('map') : ui('map')
             ,   zoom = saved ? saved.zoom || ui('zoom') : ui('zoom')
-            ,   path = "http://maps.google.com/maps/api/staticmap?center=";
+            ,   path = "http://datafluency.com/geo/info/";
 			
             navigator.geolocation.getCurrentPosition(function (position) {
-                var location = "" + position.coords.latitude + "," + position.coords.longitude;
-                path += location + "&zoom=" + zoom;
-                path += "&size=250x250&maptype=" + map + "&markers=color:red|label:P|";
-                path += location + "&sensor=false";
-
-                x$('img#static_map').attr('src', path);
+                var location = "" + position.coords.latitude + "/" + position.coords.longitude;
+                path += location;
+                x$('#positioner').attr('value', path);
             }, function () {
-                x$('img#static_map').attr('src', "assets/img/gpsfailed.png");
+                x$('#positioner').attr('value', "Unable to determine position...");
             });
         });
-    });
-    when('#save', function () {
-        store.save({
-            key:'config',
-            map:ui('map'),
-            zoom:ui('zoom')
-        });
-        display('#welcome');
     });
 });
